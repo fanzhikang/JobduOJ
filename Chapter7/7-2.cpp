a9654f2b29ef04c758494cdb0ec1e2f2aa4fbd67
Fan Zhikang <852672780@qq.com>
2019-03-19 18:56:59 +0800

    add F[i-2]

--- Chapter7/7-2.cpp
+++ Chapter7/7-2.cpp
@@ -4,7 +4,7 @@ int main(){
     F[1] = 0;
     F[2] = 1;
     for (int i=3;i<=20;i++){
-        F[i] = F[i-1]*(i-1);
+        F[i] = F[i-1]*(i-1) + (i-1)*F[i-2];
     }
     int n;
     while(scanf("%d",&n)!=EOF){
